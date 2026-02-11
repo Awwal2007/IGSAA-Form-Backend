d45cf6994566478fc0fb7ca6ec3a6357cba7bb82
village-coders <villagecoders7@gmail.com>
2026-02-11 16:58:14 +0100

    added trust proxy, 1

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Security middleware
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
